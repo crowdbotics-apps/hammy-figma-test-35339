9cb66be0d3b1f129eaff2bceebeb4217feb8873e
Crowdbotics <team@crowdbotics.com>
2022-05-20 12:20:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hammy_figma_test_35339/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hammy_figma_test_35339;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "hammy_figma_test_35339";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hammy_figma_test_35339",
+  url: "https://hammy_figma_test_35339.botics.co",
+  api: "https://hammy_figma_test_35339.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
